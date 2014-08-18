45b32d3c7af7617a2852efb45ebbc72415c15704
Juri <juri.strumpflohner@gmail.com>
2014-08-18 16:56:11 +0200

    add controller + viewbinding

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-var app = angular.module('gemStore', []);
+(function() {
+    var app = angular.module('gemStore', []);
+
+    app.controller('StoreController', function(){
+        this.product = gem;
+    });
+
+    var gem = {
+        name: 'Azurite',
+        price: 110.50,
+        canPurchase: false,
+        soldOut: true
+    };
+})();
