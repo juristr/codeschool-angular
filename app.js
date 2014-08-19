bf873a1c21a5277245e072a7b2b5cf1d27ad5a32
Juri <juri.strumpflohner@gmail.com>
2014-08-19 16:36:26 +0200

    refactors reviews into separate directive

--- app.js
+++ app.js
@@ -59,6 +59,13 @@
       };
     });
 
+    app.directive("productReviews", function() {
+      return {
+        restrict: 'E',
+        templateUrl: "product-reviews.html"
+      };
+    });
+
     var gems = [{
       name: 'Azurite',
       description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
