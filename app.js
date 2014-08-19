8d5cc9296b21449af981a54977a60202202537d4
Juri <juri.strumpflohner@gmail.com>
2014-08-19 16:52:27 +0200

    extracts product related files into separate ng module

--- app.js
+++ app.js
@@ -1,42 +1,9 @@
 (function() {
-    var app = angular.module('gemStore', []);
+    var app = angular.module('gemStore', ['store-directives']);
 
     app.controller('StoreController', function() {
         var vm = this;
         vm.products = gems;
-    });
-
-    app.directive('productGallery', function(){
-      return {
-        restrict: 'E',
-        templateUrl: 'product-gallery.html',
-        controller: function(){
-          this.current = 0;
-          this.setCurrent = function(imageNumber){
-            this.current = imageNumber || 0;
-          };
-        },
-        controllerAs: 'gallery'
-      };
-    });
-
-    app.directive("productTabs", function(){
-      return {
-        restrict: 'E',
-        templateUrl: 'product-tabs.html',
-        controller: function(){
-          this.tab = 1;
-
-          this.isSet = function(checkTab) {
-            return this.tab === checkTab;
-          };
-
-          this.setTab = function(setTab) {
-            this.tab = setTab;
-          };
-        },
-        controllerAs: 'tab'
-      };
     });
 
     app.controller('ReviewController', function(){
@@ -49,27 +16,6 @@
             product.reviews.push(this.review);
             this.review = {};
         }
-    });
-
-    app.directive('productDescription', function(){
-      return {
-        restrict: 'E',
-        templateUrl: 'product-description.html'
-      };
-    });
-
-    app.directive("productSpecs", function(){
-      return {
-        restrict: 'A',
-        templateUrl: 'product-specs.html'
-      };
-    });
-
-    app.directive("productReviews", function() {
-      return {
-        restrict: 'E',
-        templateUrl: "product-reviews.html"
-      };
     });
 
     var gems = [{
