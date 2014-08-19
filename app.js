ef6153887ee64651db80c1e0e5369b673ae53986
Juri <juri.strumpflohner@gmail.com>
2014-08-19 14:02:08 +0200

    adds custom directive product-description

--- app.js
+++ app.js
@@ -45,6 +45,13 @@
         }
     });
 
+    app.directive('productDescription', function(){
+      return {
+        restrict: 'E',
+        templateUrl: 'product-description.html'
+      };
+    });
+
     var gems = [{
       name: 'Azurite',
       description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
